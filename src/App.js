4a60f26d900580e692ba7df01fb3b62cc8a2ab04
mihkelvali <mihkel.vali@hotmail.com>
2023-11-13 20:16:44 +0200

    koduse algus

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
 
+const RAAMATUD = [
+  {
+    id: 0,
+    pealkiri: 'Raamat 1',
+    autor: 'Keegi Paul',
+    aasta: '10',
+    kirjeldus: 'asdf',
+    // pilt
+  },
+  {
+    id: 1,
+    pealkiri: 'Raamat 2',
+    autor: 'Keegi Paul',
+    aasta: '10',
+    kirjeldus: 'asdf',
+    // pilt
+  },
+  {
+    id: 2,
+    pealkiri: 'Raamat 3',
+    autor: 'Keegi Paul',
+    aasta: '10',
+    kirjeldus: 'asdf',
+    // pilt
+  },
+]
+
 function App() {
+  const [valitudRaamatuId, setValitudRaamatuId] = useState(0);
+
+  const muudaValitudRaamatuId = () => {
+    return;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" style={{ display: 'flex', flexDirection: 'row', gap: 20 }}>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>{
+          RAAMATUD.map((raamat) => (
+            <a key={raamat.pealkiri} onClick={muudaValitudRaamatuId(raamat.id)} >{raamat.pealkiri}</a>
+          ))
+        }</div>
+
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <span>Pealkiri: { RAAMATUD[valitudRaamatuId].pealkiri }</span>
+          <span>Autor: { RAAMATUD[valitudRaamatuId].autor }</span>
+        </div>
+        {/* <RaamatuDetail /> */}
       </header>
     </div>
   );
